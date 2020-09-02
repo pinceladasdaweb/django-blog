083f80c92578b89bfa0e92ec7842b7ddf7c18b7e
Pedro Rogério <pinceladasdaweb@hotmail.com>
2020-09-02 08:56:19 -0300

    Created posts view

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import UpdateView 
+from django.views.generic.edit import UpdateView
+from .models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 6
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(published=True)
+        qs = qs.annotate(
+            count_comments = Count(
+                Case(
+                    When(
+                        comment__published=True, then=1
+                    )
+                )
+            )
+        )
+
+        return qs
 
 
 class PostSearch(PostIndex):
